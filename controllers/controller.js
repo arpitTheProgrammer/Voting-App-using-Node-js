820025528e82c09785fce1a619a12f39ccfe2d87
arpitTheProgrammer <arpitsrivastava8863.9h@gmail.com>
2025-02-16 19:18:57 +0530

    Voting Application

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,15 +2,15 @@ const User = require('../Models/user')
 const Candidate = require('../Models/candidate')
 const bcrypt = require('bcrypt');
 
-const HandleSaveUser = async(req, res) =>{
+const HandleSaveUser = async (req, res) => {
     try {
         const { fullName, email, password, age } = req.body;
-            console.log((req.body))
+        console.log((req.body))
         const newUser = await User.create({
-          fullName: fullName,
-          email: email,
-          password: password,
-          age: age
+            fullName: fullName,
+            email: email,
+            password: password,
+            age: age
         })
         const savedUser = await newUser.save();
         console.log(savedUser);
@@ -19,13 +19,13 @@ const HandleSaveUser = async(req, res) =>{
         console.error('Error creating user:', err);
         return res.status(500).json({ msg: "Failed to create user", error: err.message });
     }
-        // return res.redirect('/')
-    }
+    // return res.redirect('/')
+}
 
 
-    const HandleCandidateRegister = async(req, res)=> {
-        try{
-        const {fullName, party, password, age} = req.body;
+const HandleCandidateRegister = async (req, res) => {
+    try {
+        const { fullName, party, password, age } = req.body;
         console.log(req.body)
         const newCandidate = await Candidate.create({
             fullName: fullName,
@@ -35,42 +35,42 @@ const HandleSaveUser = async(req, res) =>{
         })
         const saveCandidate = newCandidate.save()
         console.log(saveCandidate)
-        return res.json({message: "SUCCESS"})
-    } catch(error){
-        return res.json({msg: "Failed To create Candidate"})
+        return res.json({ message: "SUCCESS" })
+    } catch (error) {
+        return res.json({ msg: "Failed To create Candidate" })
     }
+}
+const HandleLogin = async (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password)
+    const user = await User.findOne({ email })
+    if (!user) {
+        return res.json({ message: "USER NOT EXISTS" })
     }
-    const HandleLogin = async(req, res) => {
-        const {email, password} = req.body;
-        console.log(email, password)
-        const user = await User.findOne({email})
-        if(!user){
-            return res.json({message: "USER NOT EXISTS"})
-        }
-        // const isCorrectPass = await bcrypt.compare(password, user.password)
-        if(password != user.password){
-            return res.json({message: "INVALID PASSWORD"})
-        }
-            user.isLoggedin =  true;
-        return res.render('candidateList')
+    // const isCorrectPass = await bcrypt.compare(password, user.password)
+    if (password != user.password) {
+        return res.json({ message: "INVALID PASSWORD" })
     }
-    const HandleCandidateLogin = async(req, res) => {
-        const {party, password} = req.body;
-        console.log(party, password);
-        const candidates = await Candidate.findOne({party})
-        if(!candidates){
-            return res.json({message: "Candidate Not exist"})
-        } 
-        if(password != candidates.password){
-            return res.json({message: "INCORRECT PASSWORD"})
-        }
-        candidates.isLoggedIn =  true;
-        return res.json({message: "LOGIN SUCCESS"})
+    user.isLoggedin = true;
+    return res.render('candidateList')
+}
+
+const HandleCandidateLogin = async (req, res) => {
+    const { party, password } = req.body;
+    console.log(party, password);
+    const candidates = await Candidate.findOne({ party })
+    if (!candidates) {
+        return res.json({ message: "Candidate Not exist" })
     }
+    if (password != candidates.password) {
+        return res.json({ message: "INCORRECT PASSWORD" })
+    }
+    candidates.isLoggedIn = true;
+    return res.json({ message: "LOGIN SUCCESS" })
+}
 module.exports = {
     HandleSaveUser,
     HandleLogin,
     HandleCandidateRegister,
     HandleCandidateLogin
 }
-    
